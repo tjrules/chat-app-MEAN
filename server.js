53a198711534e099406ae90fab98860b99d7b19b
TJ Stubbs <stubbsrule@gmail.com>
2018-03-31 12:19:12 -0400

    intialized project and created server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app)
+const PORT = process.env.PORT || 3000;
+const io = require('socket.io')(http)
+
+// displays index.html file on root of localhost
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('user connected');
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect',() =>{
+    console.log('user disconnected');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`here we come on PORT ${PORT}`);
+});
